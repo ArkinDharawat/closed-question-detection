a71dfcefa151fe85e48a44c7c9c12120f5f4eed7
ArkinDharawat <arkinrd2@illinois.edu>
2020-10-19 20:47:21 -0700

    don't split words

--- data_augmentation.py
+++ data_augmentation.py
@@ -11,25 +11,38 @@ Download w2v embs
 - gdown --id  0B7XkCwpI5KDYNlNUTTlSS21pQmM
 """
 MODEL_DIR = "./"  # path to google. glove and other embs models
+DEL_SINGLE = True
+USE_W2V = False
 
 
 def get_augmentations(max_words_to_augment=2, aug_percentage=0.01, w2v_top_k=5):
     word2vec_model_path = os.path.join(MODEL_DIR, 'GoogleNews-vectors-negative300.bin')
+
+    augmentations = []
     aug_random_swap = naw.RandomWordAug(action="swap", aug_max=max_words_to_augment, aug_p=aug_percentage)
-    aug_delete_swap = naw.RandomWordAug(action="delete", aug_max=max_words_to_augment, aug_p=aug_percentage)
-    # aug_w2v = naw.WordEmbsAug(model_type='word2vec', model_path=word2vec_model_path, action="substitute", top_k=w2v_top_k)
-    # return [aug_w2v, aug_random_swap, aug_delete_swap]
-    # TODO: add word 2 vec later
-    return [aug_random_swap, aug_delete_swap]
+    augmentations.append(aug_random_swap)
+    if DEL_SINGLE:
+        # delete only a single token
+        aug_delete_swap = naw.RandomWordAug(action="delete", aug_max=1, aug_p=aug_percentage)
+    else:
+        aug_delete_swap = naw.RandomWordAug(action="delete", aug_max=max_words_to_augment, aug_p=aug_percentage)
+    augmentations.append(aug_delete_swap)
+
+    if USE_W2V:
+        aug_w2v = naw.WordEmbsAug(model_type='word2vec', model_path=word2vec_model_path, action="substitute",
+                                  top_k=w2v_top_k)
+        augmentations.append(aug_w2v)
+
+    return augmentations
 
 
 def augment_text(text, num_aug, aug_prob, max_words):
-    augmented_text = ' '.join(text)
+    augmented_text = text.replace('|', ' ')
     augmentations_list = get_augmentations(max_words_to_augment=max_words, aug_percentage=aug_prob)
     augmentations = np.random.choice(augmentations_list, replace=True, size=num_aug)
     for aug in augmentations:
         augmented_text = aug.augment(augmented_text, n=1, num_thread=1)
-    return augmented_text.split(' ')
+    return augmented_text
 
 
 def main():
@@ -42,7 +55,8 @@ def main():
     df = pd.read_csv(os.path.join(FOLDER_PATH, 'so_questions_cleaned.csv'))
 
     pos_index = df[df['label'] > 0].index  # postive labels
-    q_titles = df['title'].apply(lambda x: x.split('|')).iloc[pos_index]
+    # q_titles = df['title'].apply(lambda x: x.split('|')).iloc[pos_index]
+    q_titles = df['title'].iloc[pos_index]
 
     augmented_titles = Parallel(n_jobs=8, backend="multiprocessing")(
         delayed(augment_text)(title, num_aug, aug_prob, max_words) for title in q_titles)
