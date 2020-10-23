8d6f003d68f8f98adf1ff744d1e28cf9f596f548
ArkinDharawat <arkinrd2@illinois.edu>
2020-10-23 07:45:55 -0700

    added gridh search cv

--- ml_models/random_forest.py
+++ ml_models/random_forest.py
@@ -3,12 +3,13 @@ import os
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
 from sklearn.utils.class_weight import compute_class_weight
+
 from ml_models.tfidf_vectorize import build_tfidf_vectorizer
 from model_metrics import get_metrics
-from sklearn.model_selection import cross_val_score
-from sklearn.metrics import classification_report
 
 FOLDER_PATH = "../so_dataset"
 
@@ -17,6 +18,7 @@ def train_model():
     # TODO: tune model
     random_seed = 42
     train_test_split_ratio = 0.8
+    hyperparam_tune = True
 
     df = pd.read_csv(os.path.join(FOLDER_PATH, 'so_questions_cleaned.csv'))
 
@@ -50,17 +52,28 @@ def train_model():
     class_weights[0] = class_weights[0]  # * 0.01  # for 0th class
     class_weights = dict(zip(np.unique(y_train), class_weights))
     print(f"Class weights {class_weights}")
-    clf = RandomForestClassifier(n_estimators=500,
-                                 random_state=random_seed,
-                                 n_jobs=-1,
-                                 class_weight='balanced_subsample',
-                                 verbose=1)
-    clf.fit(X_train, y_train)
-    print(clf.classes_, clf.class_weight)
+    if hyperparam_tune:
+        model = RandomForestClassifier()
+        tuning_parameters = {
+             'n_estimators': [100, 500, 1000, 2500],
+             'random_state': [42, 12345, 10101], # seed as hyperparamerter?
+             'class_weight': ['balanced', 'balanced_subsample'],
+             'max_features': ['auto', 'sqrt']
+        }
+        clf = GridSearchCV(model,
+                           tuning_parameters,
+                           scoring='%s_macro' % "F1",
+                           n_jobs=-1)
+        means = clf.cv_results_['mean_test_score']
+        print(f"Mean F1 macro {means}")
 
-    # k-fold CV
-    scores = cross_val_score(clf, X, y, cv=5, scoring='f1_macro')
-    print(f"CV Mean F1-macro {np.mean(scores)}")
+    else:
+        clf = RandomForestClassifier(n_estimators=500,
+                                     random_state=random_seed,
+                                     n_jobs=-1,
+                                     class_weight='balanced_subsample',
+                                     verbose=1)
+        clf.fit(X_train, y_train)
 
     # test
     y_pred = clf.predict(X_test)
