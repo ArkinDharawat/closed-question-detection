20c7b1d3f550f27eba31d2a357f74799175a3618
ArkinDharawat <arkinrd2@illinois.edu>
2020-10-23 01:04:46 -0700

    tune rf and tfidf

--- ml_models/tfidf_vectorize.py
+++ ml_models/tfidf_vectorize.py
@@ -18,20 +18,23 @@ def build_tfidf_vectorizer():
     # title vectorizer
     title_vectorizer = TfidfVectorizer(max_features=1000,
                                        stop_words='english',
-                                       min_df=0.001)
+                                       # min_df=0.001
+                                       )
     title_vectorizer.fit(q_titles)
     save_vecotrizer('title_vectorizer.pk', title_vectorizer)
 
     # body vectorizer
     body_vectorizer = TfidfVectorizer(max_features=2000,
                                       stop_words='english',
-                                      min_df=0.001)
+                                      # min_df=0.001
+                                      )
     body_vectorizer.fit(q_bodies)
     save_vecotrizer('body_vectorizer.pk', body_vectorizer)
 
     # title vectorizer
     tag_vecotrizer = TfidfVectorizer(max_features=500,
-                                     min_df=0.01)  # top 500 tags, get rid of 10% of tags
+                                     # min_df=0.01
+                                     )  # top 500 tags, get rid of 10% of tags
     tag_vecotrizer.fit(q_tags)
     save_vecotrizer('tag_vectorizer.pk', tag_vecotrizer)
 
