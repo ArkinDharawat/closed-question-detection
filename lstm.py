14d792d36f3032e31cd98609b5d10337db3b80e6
Priyanka Agarwal <pagarwal@hmc.edu>
2020-10-23 22:18:49 -0700

    cuda

--- lstm.py
+++ lstm.py
@@ -153,7 +153,7 @@ def pred (model, test_dl):
             pred = torch.max(y_hat, 1)[1].cuda()
             y_pred.append(pred.cpu())
             y_true.append(y)
-    print(y)
+    print(y_pred)
     get_metrics(y_pred=y_pred, y_true=y_true, save_dir="./", model_name='lstm')
 
 def lstm():
