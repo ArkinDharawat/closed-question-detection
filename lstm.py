23f6d86ae9daa31b740cab337718d35b7a8bba29
ArkinDharawat <arkinrd2@illinois.edu>
2020-10-29 22:22:41 -0700

    use down sampled data

--- lstm.py
+++ lstm.py
@@ -201,7 +201,7 @@ def lstm():
 
     # read data
     FOLDER_PATH = "so_dataset"
-    df = pd.read_csv(os.path.join(FOLDER_PATH, 'so_questions_downsampled.csv'))
+    df = pd.read_csv(os.path.join(FOLDER_PATH, 'so_dataset_downsampled.csv'))
     q_bodies = df['body'].apply(lambda x: x.split('|'))
     q_titles = df['title'].apply(lambda x: x.split('|'))
     q_tags = df['tag_list'].apply(lambda x: x.split('|'))
