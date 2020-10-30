778655d1103fc6f68587f02a75dea7cbb53e4513
ArkinDharawat <arkinrd2@illinois.edu>
2020-10-29 21:00:50 -0700

    add loss to val loop

--- lstm.py
+++ lstm.py
@@ -19,6 +19,7 @@ from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 from sklearn.metrics import mean_squared_error
 from lossess.focal_loss import FocalLoss
 from sklearn.utils import class_weight
+
 '''
 from tqdm.notebook import tqdm
 from torchtext.data import Field, TabularDataset, BucketIterator
@@ -28,6 +29,7 @@ import torch.optim as optim
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 import seaborn as sns
 '''
+
 
 class LSTM(nn.Module):
 
@@ -43,15 +45,14 @@ class LSTM(nn.Module):
                             bidirectional=True)
         self.drop = nn.Dropout(p=0.5)
 
-        self.fc = nn.Linear(2*dimension, 5)
+        self.fc = nn.Linear(2 * dimension, 5)
 
     def forward(self, text, text_len):
-
-        text_emb = self.embedding(text).cuda() # throw in raw text
+        text_emb = self.embedding(text).cuda()  # throw in raw text
 
         packed_input = pack_padded_sequence(text_emb, text_len, batch_first=True, enforce_sorted=False)
         packed_output, _ = self.lstm(packed_input)
-        output, _ = pad_packed_sequence(packed_output, batch_first=True) # better to pack the text
+        output, _ = pad_packed_sequence(packed_output, batch_first=True)  # better to pack the text
 
         out_forward = output[range(len(output)), text_len - 1, :self.dimension]
         out_reverse = output[:, 0, self.dimension:]
@@ -59,10 +60,11 @@ class LSTM(nn.Module):
         text_fea = self.drop(out_reduced)
 
         text_fea = self.fc(text_fea)
-        #text_fea = torch.squeeze(text_fea, 1)
-        #text_out = torch.sigmoid(text_fea)
+        # text_fea = torch.squeeze(text_fea, 1)
+        # text_out = torch.sigmoid(text_fea)
 
         return text_fea
+
 
 class ValDataset(Dataset):
     def __init__(self, X, Y):
@@ -75,8 +77,9 @@ class ValDataset(Dataset):
     def __getitem__(self, idx):
         return torch.from_numpy(self.X.iloc[idx][0].astype(np.int32)), self.y.iloc[idx], self.X.iloc[idx][1]
 
-class LSTM_variable_input(torch.nn.Module) :
-    def __init__(self, vocab_size, embedding_dim, hidden_dim) :
+
+class LSTM_variable_input(torch.nn.Module):
+    def __init__(self, vocab_size, embedding_dim, hidden_dim):
         super().__init__()
         self.hidden_dim = hidden_dim
         self.dropout = nn.Dropout(0.3)
@@ -92,6 +95,7 @@ class LSTM_variable_input(torch.nn.Module) :
         out = self.linear(ht[-1]).cuda()
         return out
 
+
 def encode_sentence(text, vocab2index, N=250):
     encoded = np.zeros(N, dtype=int)
     enc1 = np.array([vocab2index.get(word, vocab2index["UNK"]) for word in text])
@@ -99,6 +103,7 @@ def encode_sentence(text, vocab2index, N=250):
     encoded[:length] = enc1[:length]
     if length > 0:
         return encoded, length
+
 
 def train_model(model, train_dl, valid_dl, test_dl, epochs=10, lr=0.001, criterion=None):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -109,6 +114,7 @@ def train_model(model, train_dl, valid_dl, test_dl, epochs=10, lr=0.001, criteri
         print("Cannot Train Model if Loss is None")
         return
     optimizer = torch.optim.Adam(parameters, lr=lr)
+    print("Training model...")
     for i in range(epochs):
         model.train()
         sum_loss = 0.0
@@ -121,14 +127,19 @@ def train_model(model, train_dl, valid_dl, test_dl, epochs=10, lr=0.001, criteri
             loss = criterion(y_pred, y)
             loss.backward()
             optimizer.step()
-            sum_loss += loss.item()*y.shape[0]
+            sum_loss += loss.item() * y.shape[0]
             total += y.shape[0]
-        val_loss, val_acc, val_rmse = validation_metrics(model, valid_dl)
-        #if i % 5 == 1:
-        print("train loss %.3f, val loss %.3f, val accuracy %.3f, and val rmse %.3f" % (sum_loss/total, val_loss, val_acc, val_rmse))
-    validation_metrics(model, test_dl, final = True, criterion=criterion)
+        val_loss, val_acc, val_rmse = validation_metrics(model, valid_dl, criterion=criterion)
+        # if i % 5 == 1:
+        print("train loss %.3f, val loss %.3f, val accuracy %.3f, and val rmse %.3f" % (
+        sum_loss / total, val_loss, val_acc, val_rmse))
+    validation_metrics(model, test_dl, test_data=True, criterion=criterion)
 
-def validation_metrics (model, valid_dl, final = False, criterion=None):
+
+def validation_metrics(model, valid_dl, test_data=False, criterion=None):
+    if criterion is None:
+        print("Cannot Eval Model if Loss is None")
+        return
     model.eval()
     correct = 0
     total = 0
@@ -146,14 +157,15 @@ def validation_metrics (model, valid_dl, final = False, criterion=None):
         y_true.append(y)
         correct += (pred == y).float().sum()
         total += y.shape[0]
-        sum_loss += loss.item()*y.shape[0]
-        sum_rmse += np.sqrt(mean_squared_error(pred.cpu(), y.unsqueeze(-1).cpu()))*y.cpu().shape[0]
-    if final:
+        sum_loss += loss.item() * y.shape[0]
+        sum_rmse += np.sqrt(mean_squared_error(pred.cpu(), y.unsqueeze(-1).cpu())) * y.cpu().shape[0]
+    if test_data:
         get_metrics(y_pred=y_pred, y_true=y_true, save_dir="./", model_name='lstm')
     else:
-        return sum_loss/total, correct/total, sum_rmse/total
+        return sum_loss / total, correct / total, sum_rmse / total
 
-def pred (model, test_dl):
+
+def pred(model, test_dl):
     model.cuda()
     model.eval()
     y_pred = []
@@ -170,6 +182,7 @@ def pred (model, test_dl):
     print(y_true)
     get_metrics(y_pred=y_pred, y_true=y_true, save_dir="./", model_name='lstm')
 
+
 def lstm():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(device)
@@ -177,7 +190,7 @@ def lstm():
     random_seed = 42
     train_test_split_ratio = 0.2
     train_val_split_ratio = .1
-    loss = 'CE' # 'FL', 'WCE'
+    loss = 'CE'  # 'FL', 'WCE'
 
     # read data
     FOLDER_PATH = "so_dataset"
@@ -191,9 +204,8 @@ def lstm():
     for rows in q_bodies:
         counts.update(rows)
 
-
-    #creating vocabulary
-    vocab2index = {"":0, "UNK":1}
+    # creating vocabulary
+    vocab2index = {"": 0, "UNK": 1}
     words = ["", "UNK"]
     for word in counts:
         vocab2index[word] = len(words)
@@ -203,14 +215,14 @@ def lstm():
     q_bodies.append(q_titles)
     q_bodies.append(q_tags)
 
-    X = q_bodies.apply(lambda x: np.array(encode_sentence(x,vocab2index )))
+    X = q_bodies.apply(lambda x: np.array(encode_sentence(x, vocab2index)))
     y = labels
 
     # train-val-test split
     X_training, X_test, y_training, y_test = train_test_split(X, y, test_size=train_test_split_ratio,
-                                                        random_state=random_seed)
+                                                              random_state=random_seed)
     X_train, X_val, y_train, y_val = train_test_split(X_training, y_training, test_size=train_val_split_ratio,
-                                                        random_state=random_seed)
+                                                      random_state=random_seed)
 
     X_train.reset_index(drop=True)
     X_val.reset_index(drop=True)
@@ -223,14 +235,14 @@ def lstm():
     train_ds = ValDataset(X_train, y_train)
     valid_ds = ValDataset(X_val, y_val)
     test_ds = ValDataset(X_test, y_test)
-    train_dl = DataLoader(train_ds, batch_size = 16, shuffle=True)
+    train_dl = DataLoader(train_ds, batch_size=16, shuffle=True)
     val_dl = DataLoader(valid_ds)
     test_dl = DataLoader(test_ds)
 
     if loss == 'WCE':
         # TODO: Add other weighting formulas
-        class_weights = class_weight.compute_class_weight('balanced', np.unique(labels), labels) # make class-weight
-        label_weights = torch.Tensor(class_weights).to(device) # make torch tensor
+        class_weights = class_weight.compute_class_weight('balanced', np.unique(labels), labels)  # make class-weight
+        label_weights = torch.Tensor(class_weights).to(device)  # make torch tensor
         criterion = nn.CrossEntropyLoss(weight=label_weights)
     elif loss == 'FL':
         # TODO: add value for alpha and other
@@ -240,6 +252,7 @@ def lstm():
 
     model = LSTM(len(vocab2index)).to(device)
     train_model(model, train_dl, val_dl, test_dl, epochs=1, lr=0.01, criterion=criterion)
+
 
 if __name__ == '__main__':
     lstm()
