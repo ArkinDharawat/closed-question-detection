56a7460bda3b689b42d5c743a0cb304dc2f9f5b2
ArkinDharawat <arkinrd2@illinois.edu>
2020-10-31 00:18:55 -0700

    diff weighting strategy

--- ml_models/random_forest.py
+++ ml_models/random_forest.py
@@ -55,7 +55,7 @@ def train_model():
 
     # train
     # class weights configured manually
-    class_weights = calculate_class_weights('sklearn')
+    class_weights = calculate_class_weights(y, 'sklearn')
 
     if hyperparam_tune:
         model = RandomForestClassifier()
