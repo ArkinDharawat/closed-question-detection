ab90101e5e17b1f2e555380111f7a93a0d63ff25
ArkinDharawat <arkinrd2@illinois.edu>
2020-11-09 22:43:17 -0800

    adjust fl

--- train.py
+++ train.py
@@ -302,10 +302,11 @@ def run():
         print(f"Weights are {label_weights}")
         criterion = nn.CrossEntropyLoss(weight=label_weights)
     elif loss == 'FL':
+        #  gamma = 0.5 ->
         # gamma = 1 -> not working well
         # gamma = 2, alpha = 0.25 ->
         # gamma = 5 ->
-        criterion = FocalLoss(alpha=1.0, gamma=2, smooth=1e-5)
+        criterion = FocalLoss(alpha=1.0, gamma=0.5, smooth=1e-5)
     else:
         criterion = nn.CrossEntropyLoss()
 
