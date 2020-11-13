4df5d7d405264f763c87b8e1d5a9d384ad99f1a0
ArkinDharawat <arkinrd2@illinois.edu>
2020-11-13 00:57:13 -0800

    cleaned up comments

--- train.py
+++ train.py
@@ -318,14 +318,16 @@ def run():
         model = LSTM(embedding=embedding, emb_dim=embedding_dim, dimension=256, num_layers=2)
     elif model_type == 'BERT':
         # model = AutoModelForTokenClassification.from_pretrained("lanwuwei/BERTOverflow_stackoverflow_github")
-        model = BERTClassifier(hidden_dim=128, dropout=0.3)
+        model = BERTClassifier(hidden_dim=256, dropout=0.5)
     train_model(model, train_dl, val_dl, test_dl, epochs=epochs, lr=learning_rate, criterion=criterion)
     """
     Best so far:
     max length = 32
     python3 train.py --seed 12345 --loss WCE --epochs 10 --batch_size 64 --lr 2e-5 --model BERT
     max length = 64
-    
+    python3 train.py --seed 12345 --loss WCE --epochs 9 --batch_size 64 --lr 2e-5 --model BERT
+    droput=0.3
+    python3 train.py --seed 12345 --loss WCE --epochs 5 --batch_size 64 --lr 2e-5 --model BERT
     """
 
 if __name__ == '__main__':
