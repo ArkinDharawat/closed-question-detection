6d610b10b44dcb788be122248733f85a9415b480
ArkinDharawat <arkinrd2@illinois.edu>
2020-11-14 14:02:58 -0800

    lalbel count and sum

--- data_visualization/label_counts.py
+++ data_visualization/label_counts.py
@@ -6,3 +6,4 @@ FOLDER_PATH = "../so_dataset"
 if __name__ == '__main__':
     df = pd.read_csv(os.path.join(FOLDER_PATH, "so_questions_labelled.csv"))
     print(df['Label'].value_counts())
+    print(sum(df['Label'].value_counts()))
