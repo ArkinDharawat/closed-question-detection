7657aff25dda8b2a5cf86d891876139d68ae5ec5
ArkinDharawat <arkinrd2@illinois.edu>
2020-11-13 23:42:32 -0800

    use title

--- train.py
+++ train.py
@@ -253,7 +253,7 @@ def run():
         # q_bodies.append(q_tags)
         X = q_titles.apply(lambda x: np.array(encode_sentence(x, vocab2index)))
     elif model_type == "BERT":
-        X = q_titles + q_bodies
+        X = q_titles + q_bodies + q_titles
 
     y = labels
 
