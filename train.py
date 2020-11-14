aeb45129bc35d23c1df541ff3dfa3dfc984c8bb9
ArkinDharawat <arkinrd2@illinois.edu>
2020-11-14 01:20:23 -0800

    inc hidden dim

--- train.py
+++ train.py
@@ -240,7 +240,7 @@ def run():
         # q_bodies.append(q_tags)
         X = q_titles.apply(lambda x: np.array(encode_sentence(x, vocab2index)))
     elif model_type == "BERT":
-        X = q_titles + q_bodies + q_tags
+        X = q_titles + q_bodies # + q_tags
 
     y = labels
 
@@ -305,7 +305,7 @@ def run():
         model = LSTM(embedding=embedding, emb_dim=embedding_dim, dimension=256, num_layers=2)
     elif model_type == 'BERT':
         # model = AutoModelForTokenClassification.from_pretrained("lanwuwei/BERTOverflow_stackoverflow_github")
-        model = BERTClassifier(hidden_dim=256, dropout=0.5)
+        model = BERTClassifier(hidden_dim=512, dropout=0.5)
     # TODO: Should be validation dl
     train_model(model, train_dl, test_dl, test_dl, epochs=epochs, lr=learning_rate, criterion=criterion)
     """
