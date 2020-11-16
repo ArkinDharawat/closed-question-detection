7762c43e44badb165f968b17d83ae987bff669e0
Aaron Li <aaronli9@stanford.edu>
2020-11-15 22:54:44 -0800

    changed file path

--- data_visualization/sample_text.py
+++ data_visualization/sample_text.py
@@ -6,7 +6,7 @@ import pandas as pd
 FOLDER_PATH = "so_dataset"
 
 if __name__ == '__main__':
-    df = pd.read_csv(os.path.join(FOLDER_PATH, "so_questions_cleaned_rm_stopw.csv"))
+    df = pd.read_csv(os.path.join(FOLDER_PATH, "so_questions_labelled.csv"))
     label_types = df['label'].unique()
     sample_size = 3
 
